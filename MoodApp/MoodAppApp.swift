979c0b83c25bbad27754160747aa403f3130fc06
Jin-Mach <ji82ma@seznam.cz>
2025-12-03 19:39:59 +0100

    Initial Commit

---
+++ MoodApp/MoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodAppApp.swift
+//  MoodApp
+//
+//  Created by Jindřich Machytka on 03.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct MoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoodAppTests/MoodAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoodAppTests.swift
+//  MoodAppTests
+//
+//  Created by Jindřich Machytka on 03.12.2025.
+//
+
+import Testing
+@testable import MoodApp
+
+struct MoodAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
